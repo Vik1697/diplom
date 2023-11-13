d33b4192509b9fa5a775bf67f0081c0340458991
Vik1697 <150597964+Vik1697@users.noreply.github.com>
2023-11-13 15:28:49 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = " https://c72a0ccb-3094-4330-b067-c783569cb292.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Vika",
+    "lastName": "Fog",
+    "address": "Koneva, 5 apt.",
+    "metroStation": 4,
+    "phone": "+7 999 700 22 22",
+    "rentTime": 5,
+    "deliveryDate": "2020-12-12",
+    "comment": "Kak mozhno skoree",
+    "color": [
+        "BLACK"
+    ]
+}
